729b84f48008d2944eeae730093161fa8dedaea4
Gabriel Silveira <ramos97033@outlook.com>
2025-06-11 22:00:15 -0300

    editing style

--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -6,7 +6,7 @@ const OrderPage = () => {
     <main className={styles.main}>
       <h1 className={styles.title}>Página de Pedidos</h1>
       <p className={styles.subtitle}>
-        CRUD Completo de Pedidos com Next.js, TypeScript, Tailwind CSS, .NET 8, Entity Framework Core e Posgres
+        CRUD Completo de Pedidos com <span className="font-extrabold">Next.js </span>, <span className="font-extrabold">TypeScript</span>, <span className="font-extrabold">Tailwind CSS</span>, <span className="font-extrabold">.NET 8</span>, <span className="font-extrabold">Entity Framework Core</span> e <span className="font-extrabold">Postgres</span>
       </p>
       <div className={styles.ordersTableWrapper}>
         <OrdersTable />
